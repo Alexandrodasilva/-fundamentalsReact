b63dfd7c3a6bf2db5f3a1a18c5a15949155222f4
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2022-12-09 21:26:34 -0300

    inserindo css e configurando essa parte

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import reactDOM from 'react-dom' //importar o react-dom e o reactaDom é que vai ser usado
-
+import React from 'react'
+import './index.css'
 // const el = document.getElementById('root')
 // reactDOM.render('Olá React', el) // o método render para renderizar na tela
  // o primeiro parametro é para renderizar na tela 
  // e o segundo elemento é o que vai ser chamado para setar na pag 
 
 //
-
+const log = <strong>Olá React</strong>
 reactDOM.render(
-    'Olá React', 
+    <div> 
+        <strong>
+            {log /*par de chaves é para interpetrar a tag e pegar o valor da cons*/}       
+        </strong> t
+    </div>, 
     document.getElementById('root')
 )
 
