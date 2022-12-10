cb365ad5f9e0d59cfe7de85f071955e541078cc1
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2022-12-09 21:11:35 -0300

    inserindo strings e deletando comentarios

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-import 'react-dom'
+import reactDOM from 'react-dom' //importar o react-dom e o reactaDom é que vai ser usado
 
+// const el = document.getElementById('root')
+// reactDOM.render('Olá React', el) // o método render para renderizar na tela
+ // o primeiro parametro é para renderizar na tela 
+ // e o segundo elemento é o que vai ser chamado para setar na pag 
+
+//
+
+reactDOM.render(
+    'Olá React', 
+    document.getElementById('root')
+)
+
+//outra forma de fazer uma chamada do root 
