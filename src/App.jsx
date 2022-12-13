56dbf427030dd8a956d98d089f81edd38dc2e6d8
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2022-12-13 00:36:03 -0300

    estudo sobre cards, com aplicacao de componentes dentro de outro e chamando cada um, além de usar props. children para isso

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,33 @@ import Primeiro from './components/basicos/Primeiro'
 import ComPar from './components/basicos/ComParamentro'
 import Fragmento from './components/basicos/Fragmento'
 import Number from "./components/basicos/Number";
+import Card from "./layout/Card"
+import './App.css'
 
 export default function App(){
     return(
-        <div id="app">
+        <div className="App">
             <h1>Fundamentos React(arrow)</h1>
-            <Number numMax={24} numMin={10}></Number>
-            <Fragmento/>
-            <ComPar 
-                titulo="Segundo Comenente"
-                aluno="Alexandro" 
-                nota={9.5} />
-            <Primeiro></Primeiro>
+            <div className="Cards">
+                <Card titulo="#04 - Desafio aleatorio">
+                    <Number numMax={24} numMin={10}></Number>
+                </Card>
+
+                <Card titulo="#03 - Fragmento">
+                    <Fragmento/>
+                </Card>
+
+                <Card titulo="#02 - Com Parametro">
+                    <ComPar 
+                        titulo="Segundo Comenente"
+                        aluno="Alexandro" 
+                        nota={9.5} />
+                </Card>
+
+                <Card titulo="#01 - Primeiro componente">
+                    <Primeiro></Primeiro>
+                </Card>
+            </div>
         </div>
     );
 }
