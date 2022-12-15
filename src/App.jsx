92dd5dfa32e356443c6dfe12384322cce5d1d322
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2022-12-15 09:55:15 -0300

    amor

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ComPar from './components/basicos/ComParamentro'
 import Fragmento from './components/basicos/Fragmento'
 import Number from "./components/basicos/Number";
 import Card from "./layout/Card"
+import Familia from "./components/basicos/Familia";
+import MembroFamilia from "./components/basicos/FamiliaMembro";
 import './App.css'
 
 export default function App(){
@@ -11,22 +13,30 @@ export default function App(){
         <div className="App">
             <h1>Fundamentos React(arrow)</h1>
             <div className="Cards">
-                <Card titulo="#04 - Desafio aleatorio">
+                <Card titulo="#05 - componente filho" color="#00C8F8">
+                    <Familia sobrenome="silva" idade={12}>
+                        <MembroFamilia nome="pedro" />
+                        {/* <MembroFamilia nome="ana"/>
+                        <MembroFamilia nome="junior" /> */}
+                    </Familia>
+                </Card>
+
+                <Card titulo="#04 - Desafio aleatorio" color="#FA6900">
                     <Number numMax={24} numMin={10}></Number>
                 </Card>
 
-                <Card titulo="#03 - Fragmento">
+                <Card titulo="#03 - Fragmento" color="#E94C6F">
                     <Fragmento/>
                 </Card>
 
-                <Card titulo="#02 - Com Parametro">
+                <Card titulo="#02 - Com Parametro" color="#E8871A">
                     <ComPar 
                         titulo="Segundo Comenente"
                         aluno="Alexandro" 
                         nota={9.5} />
                 </Card>
 
-                <Card titulo="#01 - Primeiro componente">
+                <Card titulo="#01 - Primeiro componente" color="#588C73">
                     <Primeiro></Primeiro>
                 </Card>
             </div>
---
+++ src/components/basicos/Familia.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MembroFamilia from "./FamiliaMembro";
+
+export default function Familia(props){
+    return(
+        <>
+            {props.children}
+        </>
+    );
+}
