76590d0c6daa2a953ee2997244205bfd2c4c86c1
alexandrodasilva <alexsamdrosilva13@gmail.com>
2022-12-20 17:10:40 -0300

    mostrando no componente por meio do state as propriedades passadas por meio da function

--- src/components/comunicao/IndiretaFilho.jsx
+++ src/components/comunicao/IndiretaFilho.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 export default props => {
+    const min = 50;
+    const max = 70;
+    const gerarIdade  = () => parseInt(Math.random() * (max - min) + max)
     return(
         <>
             <div>filho</div>
-            <button onClick={ _ => props.quandoClicar('alexandro', 25, true)}
+            <button onClick={ _ => props.quandoClicar('alexandro', gerarIdade(), true)}
             >Fornecer informações</button>
         </>
     )
